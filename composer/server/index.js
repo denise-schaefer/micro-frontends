00e4d6bd4a239c1bb306809b092ab59032ecb815
Denise Schäfer <denise.schaefer@dm.de>
2019-03-30 16:33:29 +0100

    add composing server

---
+++ composer/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (_, res) => {
+  const html = render();
+  res.send(html);
+});
+
+app.listen(8080, () => console.log('app listening on http://localhost:8080'));
+
+function render() {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ui composing</title>
+</head>
+<body>
+
+<div id="root"></div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/16.8.5/umd/react.production.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.5/umd/react-dom.production.min.js"></script>
+
+</body>
+</html>`;
+}
