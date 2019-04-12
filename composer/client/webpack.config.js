7b585e171a21b2c6499b92557736a75f932aa07e
Denise Schäfer <denise.schaefer@dm.de>
2019-04-12 17:56:16 +0200

    fix production build

--- composer/client/webpack.config.js
+++ composer/client/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const isProd = mode === 'production';
@@ -17,7 +17,7 @@ module.exports = {
   },
   plugins: [
     !isProd &&
-      new UglifyJsPlugin({
+      new TerserPlugin({
         cache: true,
       }),
   ].filter(Boolean),
