8ea54f2bf742eedb439924390f55b156d60ce19f
Denise Schäfer <denise.schaefer@dm.de>
2019-04-19 09:29:30 +0200

    converted SearchContainer to hooks, introduced useThunkReducer

--- search/client/src/components/SearchContainer.jsx
+++ search/client/src/components/SearchContainer.jsx
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
 import qs from 'qs';
 import { getSearchProviders } from 'search-api/lib';
 import SearchResultHeader from './SearchResultHeader';
 import SearchResultBody from './SearchResultBody';
-import { doLoadData, doLoadCount, setActiveSearchProvider } from './redux/actions';
-import { structuredSelector as mapStateToProps } from './redux/reducers';
+import { doLoadData, doLoadCount, setActiveSearchProvider } from './state/actions';
 import isEmpty from '../util/isEmpty';
 import { SearchInput } from './SearchInput';
 import Container from 'react-bootstrap/Container';
@@ -14,6 +11,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import SearchFallback from './SearchFallback';
+import search from './state/reducers';
+import useThunkReducer from '../util/useThunkReducer';
 
 const readQueryDataFromUrl = () => {
   const queryString = window.location.search;
@@ -29,32 +28,27 @@ const handlePushHistory = queryData => {
   window.history.pushState({ ...queryData }, null, queryString);
 };
 
-export class UnconnectedSearchContainer extends Component {
-  componentDidMount() {
-    const queryData = readQueryDataFromUrl();
-    const searchProviders = getSearchProviders();
-    if (queryData) {
-      this.handleSearch(queryData.searchType || searchProviders[0].ID, queryData);
-    } else {
-      this.props.setActiveSearchProvider(searchProviders[0]);
-    }
-    window.addEventListener('popstate', this.handleHistoryPopState);
+export default function SearchContainer() {
+  const [state, dispatch] = useThunkReducer(search, {
+    searchState: {},
+    countState: {},
+    errorState: {},
+    queryState: {},
+    activeSearchProvider: {},
+    loadingState: false,
+    displaySuggestions: false,
+  });
+  const {
+    activeSearchProvider,
+    searchState,
+    queryState,
+    countState,
+    errorState,
+    loadingState,
+    displaySuggestions,
+  } = state;
 
-    this.unsubscribeListeners = () => {
-      window.removeEventListener('popstate', this.handleHistoryPopState);
-    };
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeListeners();
-  }
-
-  onTabClick = searchProvider => {
-    handlePushHistory(this.getQueryData(searchProvider));
-    this.handleSearch(searchProvider.ID, this.getQueryData(searchProvider));
-  };
-
-  getActiveSearchProvider = searchType => {
+  const getActiveSearchProvider = searchType => {
     const searchProviders = getSearchProviders();
 
     const activeSearchProviderFromSearchType = searchProviders.filter(
@@ -64,136 +58,119 @@ export class UnconnectedSearchContainer extends Component {
     return activeSearchProviderFromSearchType || searchProviders[0];
   };
 
-  getQueryData = searchProvider => {
-    const { queryState } = this.props;
+  const getQueryData = searchProvider => {
     return !isEmpty(queryState) && searchProvider && !isEmpty(queryState[searchProvider.ID])
       ? queryState[searchProvider.ID]
-      : this.props.queryData;
+      : null;
   };
 
-  fetchData = queryData => {
-    const searchType = queryData ? queryData.searchType : '';
-    const activeSearchProvider = this.getActiveSearchProvider(searchType);
-    handlePushHistory(queryData);
-    this.handleSearch(activeSearchProvider.ID, queryData);
-  };
-
-  handleSearch = (activeSearchProviderID, queryData) => {
+  const handleSearch = (activeSearchProviderID, queryData) => {
     const searchProviders = getSearchProviders();
 
     searchProviders.forEach(provider => {
-      this.props.loadCount({
-        searchProviderId: provider.ID,
+      dispatch(
+        doLoadCount({
+          searchProviderId: provider.ID,
+          providers: searchProviders,
+          queryData,
+        })
+      );
+    });
+
+    dispatch(
+      doLoadData({
+        searchProviderId: activeSearchProviderID,
         providers: searchProviders,
         queryData,
-      });
-    });
-
-    this.props.loadData({
-      searchProviderId: activeSearchProviderID,
-      providers: searchProviders,
-      queryData,
-    });
+      })
+    );
   };
 
-  submitSearch = (activeSearchProviderID, queryData) => {
-    this.handleSearch(activeSearchProviderID, queryData);
-    handlePushHistory({ ...queryData, searchType: activeSearchProviderID });
-  };
-
-  handleHistoryPopState = event => {
+  const handleHistoryPopState = event => {
     const newState = event.state;
 
     if (newState) {
       const searchType = newState.searchType;
       const query = newState.query;
-      const activeSearchProvider = this.getActiveSearchProvider(searchType);
-      this.handleSearch(activeSearchProvider.ID, {
-        ...this.getQueryData(activeSearchProvider),
+      const activeSearchProvider = getActiveSearchProvider(searchType);
+      handleSearch(activeSearchProvider.ID, {
+        ...getQueryData(activeSearchProvider),
         query,
       });
     } else {
       const activeSearchProvider = getSearchProviders()[0];
-      this.handleSearch(activeSearchProvider[0].ID, this.getQueryData(activeSearchProvider));
+      handleSearch(activeSearchProvider[0].ID, getQueryData(activeSearchProvider));
     }
   };
 
-  render() {
-    const {
-      activeSearchProvider,
-      searchState,
-      countState,
-      errorState,
-      loadingState,
-      displaySuggestions,
-    } = this.props;
+  const onTabClick = searchProvider => {
+    handlePushHistory(getQueryData(searchProvider));
+    handleSearch(searchProvider.ID, getQueryData(searchProvider));
+  };
 
+  const fetchData = queryData => {
+    const searchType = queryData ? queryData.searchType : '';
+    const activeSearchProvider = getActiveSearchProvider(searchType);
+    handlePushHistory(queryData);
+    handleSearch(activeSearchProvider.ID, queryData);
+  };
+
+  const submitSearch = (activeSearchProviderID, queryData) => {
+    handleSearch(activeSearchProviderID, queryData);
+    handlePushHistory({ ...queryData, searchType: activeSearchProviderID });
+  };
+
+  useEffect(() => {
     const queryData = readQueryDataFromUrl();
+    const searchProviders = getSearchProviders();
+    if (queryData) {
+      handleSearch(queryData.searchType || searchProviders[0].ID, queryData);
+    } else {
+      dispatch(setActiveSearchProvider(searchProviders[0]));
+    }
+    window.addEventListener('popstate', handleHistoryPopState);
 
-    return (
-      <Container>
-        <Navbar className="bg-light justify-content-between" style={{ marginBottom: '20px' }}>
-          <Navbar.Brand>micro-frontends</Navbar.Brand>
-          <SearchInput
-            onSubmit={query => this.submitSearch(activeSearchProvider.ID, query)}
-            query={queryData && queryData.query}
-          />
-        </Navbar>
-        {!displaySuggestions && activeSearchProvider && this.getQueryData(activeSearchProvider) && (
-          <Col style={{ justifyContent: 'center' }}>
-            <Row className="justify-content-center">
-              <SearchResultHeader
-                query={this.getQueryData(activeSearchProvider).query}
-                activeSearchProvider={activeSearchProvider}
-                countState={countState}
-                onTabClick={searchProvider => this.onTabClick(searchProvider)}
-              />
-            </Row>
-            <Row className="justify-content-center">
-              <SearchResultBody
-                activeSearchProvider={activeSearchProvider}
-                fetchData={this.fetchData}
-                errorState={errorState}
-                queryData={this.getQueryData(activeSearchProvider)}
-                searchState={searchState}
-              />
-            </Row>
-          </Col>
-        )}
-        {displaySuggestions && <SearchFallback query={queryData.query} searchState={searchState} />}
-        {loadingState && <div>Loading...</div>}
-      </Container>
-    );
-  }
+    return () => {
+      window.removeEventListener('popstate', handleHistoryPopState);
+    };
+  }, []);
+
+  const queryDataFromUrl = readQueryDataFromUrl();
+
+  return (
+    <Container>
+      <Navbar className="bg-light justify-content-between" style={{ marginBottom: '20px' }}>
+        <Navbar.Brand>micro-frontends</Navbar.Brand>
+        <SearchInput
+          onSubmit={query => submitSearch(activeSearchProvider.ID, query)}
+          query={queryDataFromUrl && queryDataFromUrl.query}
+        />
+      </Navbar>
+      {!displaySuggestions && activeSearchProvider && getQueryData(activeSearchProvider) && (
+        <Col style={{ justifyContent: 'center' }}>
+          <Row className="justify-content-center">
+            <SearchResultHeader
+              query={getQueryData(activeSearchProvider).query}
+              activeSearchProvider={activeSearchProvider}
+              countState={countState}
+              onTabClick={searchProvider => onTabClick(searchProvider)}
+            />
+          </Row>
+          <Row className="justify-content-center">
+            <SearchResultBody
+              activeSearchProvider={activeSearchProvider}
+              fetchData={fetchData}
+              errorState={errorState}
+              queryData={getQueryData(activeSearchProvider)}
+              searchState={searchState}
+            />
+          </Row>
+        </Col>
+      )}
+      {displaySuggestions && (
+        <SearchFallback query={queryDataFromUrl.query} searchState={searchState} />
+      )}
+      {loadingState && <div>Loading...</div>}
+    </Container>
+  );
 }
-
-const mapDispatchToProps = dispatch => ({
-  loadData: (...args) => doLoadData(...args)(dispatch),
-  loadCount: (...args) => doLoadCount(...args)(dispatch),
-  setActiveSearchProvider: (...args) => dispatch(setActiveSearchProvider(...args)),
-});
-
-UnconnectedSearchContainer.propTypes = {
-  // own props
-  queryData: PropTypes.shape({
-    query: PropTypes.string,
-    searchType: PropTypes.string,
-    ...PropTypes.object,
-  }).isRequired,
-  // props from store
-  searchState: PropTypes.object,
-  countState: PropTypes.object,
-  errorState: PropTypes.object,
-  queryState: PropTypes.object,
-  loadingState: PropTypes.bool,
-  displaySuggestions: PropTypes.bool,
-  activeSearchProvider: PropTypes.object,
-  loadData: PropTypes.func.isRequired,
-  loadCount: PropTypes.func.isRequired,
-  setActiveSearchProvider: PropTypes.func.isRequired,
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UnconnectedSearchContainer);
---
+++ search/client/src/util/useThunkReducer.js
@@ -0,0 +1,48 @@
+import { useCallback, useRef, useState } from 'react';
+
+/**
+ * Augments React's useReducer() hook so that the action
+ * dispatcher supports thunks.
+ *
+ * @param {Function} reducer
+ * @param {*} initialArg
+ * @param {Function} [init]
+ * @returns {[*, Dispatch]}
+ */
+function useThunkReducer(reducer, initialArg, init = a => a) {
+  const [hookState, setHookState] = useState(init(initialArg));
+
+  const state = useRef(hookState);
+  const getState = useCallback(() => state.current, [state]);
+  const setState = useCallback(
+    newState => {
+      state.current = newState;
+      setHookState(newState);
+    },
+    [state, setHookState]
+  );
+
+  const reduce = useCallback(action => reducer(getState(), action), [reducer, getState]);
+
+  /**
+   * @callback Thunk
+   * @param {Dispatch} dispatch
+   * @param {Function} getState
+   * @returns {void|*}
+   */
+
+  /**
+   * @callback Dispatch
+   * @param {Object|Thunk} action
+   * @returns {void|*}
+   */
+  const dispatch = useCallback(
+    action =>
+      typeof action === 'function' ? action(dispatch, getState) : setState(reduce(action)),
+    [getState, setState, reduce]
+  );
+
+  return [hookState, dispatch];
+}
+
+export default useThunkReducer;
