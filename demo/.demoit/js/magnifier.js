8c430fd73f0afecb8a1eb55874c21e28bca97d1f
Benjamin Seber <benjamin.seber@dm.de>
2019-04-28 16:59:41 +0200

    style adjustments for synyx beamer

--- demo/.demoit/js/magnifier.js
+++ demo/.demoit/js/magnifier.js
@@ -1,15 +1,16 @@
 const image = document.querySelector('.magnifier-content');
-const height = image.height;
-
-const magnifierHeight = 300;
-
-let y = height - magnifierHeight;
 
 image.onload = function() {
   const imageUrl = image.getAttribute('data-image');
 
   image.src = imageUrl;
   image.onload = function() {
+    const height = image.height;
+
+    const magnifierHeight = 300;
+
+    let y = height - magnifierHeight;
+
     y = y <= 0 ? height : y - 25;
     image.style.transform = `translateY(${-1 * y}px)`;
 
