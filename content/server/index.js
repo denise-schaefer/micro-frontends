6fe370a1b287029011e8143246f03915db2eb970
Denise Schäfer <denise.schaefer@dm.de>
2019-05-01 12:27:21 +0200

    fix console.log comment

--- content/server/index.js
+++ content/server/index.js
@@ -6,4 +6,4 @@ app.get('/content.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../dist/content.js'));
 });
 
-app.listen(3012, () => console.log('product app listening on port 3012'));
+app.listen(3012, () => console.log('content app listening on port 3012'));
